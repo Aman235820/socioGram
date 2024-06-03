5b16a58dfb18c24ec0937601be7798d88659dd7a
Aman Somani <amansomani@policybazaar.com>
2024-06-03 18:33:54 +0530

    add CORS policy for front-end API exposure

---
+++ src/main/java/com/aman/socialMedia/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.aman.socialMedia.config;
+
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${allowed.origin}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+
+    }
+
+
+}
